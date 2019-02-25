3713da4afb465492fed673da2655436894478737
hristiyan.iliev97 <hristiayn.iliev97@gmail.com>
2019-02-25 19:32:55 +0200

    upload files

---
+++ START_YTB_SIKULI_SCRIPT.bat
@@ -0,0 +1,3 @@
+set ROOT=%~dp0
+set PIC=%ROOT%/images/
+cmd /K %ROOT%/sikulix/runsikulix.cmd -r %ROOT%/ytb.sikuli
---
+++ ytb.sikuli/ytb.py
@@ -0,0 +1,39 @@
+import os, time
+
+picsFolder = os.environ['PIC']
+
+def open_start_menu():
+	# test function
+    click(picsFolder + 'startMenu.png')
+
+class YoutubeUnpauser:
+	__timesUnpaused = 0
+	''' Functionality to progressively reduce the time threshold for new look ups
+		for example storing the exact time of the last successful find & click operation
+		and returning a time.sleep(value) for the while loop based on that. For example:
+		__sleepThresholdAfterFindFailed = int ( 300 / ( currentTime() - __timeOfLastSuccess ) in seconds )  
+	'''
+	def __init__(self):
+		print "Created a YoutubeUnpauser instance."
+
+	def findContinueButton(self):
+		print "Looking for Youtube window message box"
+		try:
+			# look for the image
+			msgBox = find(picsFolder + 'videoPaused_messageBox.png')
+			found = msgBox.find(picsFolder + 'videoPaused_OK_button.png')
+			click (found)
+			self.__timesUnpaused += 1
+			print "Times Unpaused: " + str(self.__timesUnpaused)
+			return 5 # for the while loop time.sleep()
+		except FindFailed as ff_err:
+			# video is still playing therefore do nothing
+			print ff_err
+			return 2 # for the while loop time.sleep()
+
+
+y = YoutubeUnpauser()
+
+while True:
+	time.sleep( y.findContinueButton() )  # the window will show once per minute at most
+	continue
